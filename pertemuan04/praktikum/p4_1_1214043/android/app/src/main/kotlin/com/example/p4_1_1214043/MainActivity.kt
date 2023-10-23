b25b687ded4f339fd4fc63810d756f9ccd2b5b68
zidanputra1112 <zidanputra1112@gmail.com>
2023-10-23 16:04:47 +0700

    Praktikum Pertemuan04

---
+++ pertemuan04/praktikum/p4_1_1214043/android/app/src/main/kotlin/com/example/p4_1_1214043/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p4_1_1214043
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pertemuan04/praktikum/p4_1_1214043/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tempat Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Ranca Upas',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.calendar_today),
+                          SizedBox(height: 8.0),
+                          Text('Open Everyday'),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.access_time),
+                          SizedBox(height: 8.0),
+                          Text('09.00-22.00'),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.monetization_on),
+                          SizedBox(height: 8.0),
+                          Text('Rp.30.000'),
+                        ],
+                      ),
+                    ],
+                  )),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  'Ranca Upas Ciwidey adalah kawasan bumi perkemahan di bawah pengelolaan perhutani. Tempat ini berada di kawasan wisata Bandung Selatan, satu lokasi dengan kawah putih, kolam Cimanggu dan situ Patenggang. Banyak hal yang bisa dilakukan di kawasan wisata ini, seperti berkemah, berinteraksi dengan rusa, sampai bermain di water park dan mandi air panas.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
